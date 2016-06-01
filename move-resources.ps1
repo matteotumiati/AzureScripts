75d6a93a1a96901f6503b987f3a334e8bce9ebde
Matteo Tumiati <matteot@icubed.it>
2016-06-01 15:01:37 +0200

    Aggiunto lo script per muovere le risorse da una subscription ad un'altra.

---
+++ move-resources.ps1
@@ -0,0 +1,23 @@
+﻿$oldResourceGroupName = ""
+$newResourceGroupName = ""
+$webAppName           = ""
+$planName             = ""
+$destSubscriptionId   = ""
+
+<# Login #>
+Login-AzureRmAccount
+
+<# List all subscription (if needed) #>
+Get-AzureRmSubscription
+
+<# Select the correct subscription #>
+Select-AzureRmSubscription $destSubscriptionId
+
+<# Get WebApp #> 
+$webapp = Get-AzureRmResource -ResourceGroupName $oldResourceGroupName -ResourceName $webAppName -ResourceType Microsoft.Web/sites 
+
+<# Get ServicePlan #> 
+$plan = Get-AzureRmResource -ResourceGroupName $oldResourceGroupName -ResourceName $planName -ResourceType Microsoft.Web/serverFarms
+
+<# move resources to a new subscrption #> 
+Move-AzureRmResource -DestinationResourceGroupName $newResourceGroupName -ResourceId ($webapp.ResourceId, $plan.ResourceId) -DestinationSubscriptionId $destSubscriptionId
